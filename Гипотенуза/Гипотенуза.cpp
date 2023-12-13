11526e0ba5789898c2acc4c411b88bd1e5e1d0c5
glebs <glebs@GLEB-PC>
2023-12-13 16:35:08 +0300

    Добавьте файлы проекта.

---
+++ Гипотенуза/Гипотенуза.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	int a, b, c;
+
+	cout << "Enter first num: " << endl;
+	cin >> a;
+	
+	cout << "Enter second num: " << endl;
+	cin >> b;
+	
+	c = a * a + b * b;
+
+	cout << "hypotenuse of triangle: "<< c << endl;
+}
